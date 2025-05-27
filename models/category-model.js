5dd4ab0adaa87a528b70944429e4261f1e250ec6
JobinC <jobincbenny@gmail.com>
2025-05-27 17:03:42 +0530

    Updated custom validation check

--- models/category-model.js
+++ models/category-model.js
@@ -4,7 +4,7 @@ const AutoIncrement = require("mongoose-sequence")(mongoose)
 
 const categorySchema = new mongoose.Schema({
     _id: { type: Number },
-    category:{type:String,required:true,unique:true},
+    category:{type:String,required:true},
     userId: {type:mongoose.Schema.Types.ObjectId,unique:true,ref:"User"},
 })
 categorySchema.plugin(AutoIncrement,{inc_field:'_id'})
